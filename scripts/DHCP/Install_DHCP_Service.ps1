e70369d5f79858cbd50b68f57e2d0e56702fe897
CarrotCrusadR <andersmortsen@gmail.com>
2026-04-14 08:58:50 +0200

    Add scripts to configure DHCP scope size and install DHCP service role with authorization

--- scripts/DHCP/Install_DHCP_Service.ps1
+++ scripts/DHCP/Install_DHCP_Service.ps1
@@ -1,4 +1,5 @@
-#install the DHCP sevice role
+#install the DHCP sevice role and authorize the server to be able to manage DHCP
 if (-not (Get-WindowsFeature -Name DHCP).Installed) {
     Install-WindowsFeature -Name DHCP -IncludeManagementTools
+    Add-DhcpServerInDC -DnsName "$($env:COMPUTERNAME).$($env:DOMAINNAME)"
 }
