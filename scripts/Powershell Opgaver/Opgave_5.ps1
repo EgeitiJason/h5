d912f9527c34091fd2a2737c35f14a6245109cb7
ChellaMussen <chella@live.dk>
2026-04-15 13:45:34 +0200

    Add script to modify folder permissions for "File-Administration" group

---
+++ scripts/Powershell Opgaver/Opgave_5.ps1
@@ -0,0 +1,15 @@
+#Ændre mappe rettigheder fra everyone til kun en Security Group "File-Administration"
+$folderPath = "E:\fortrolig"
+$acl = Get-Acl $folderPath
+$acl.SetAccessRuleProtection($true, $false) # tilføjer arve rettigheder
+$acl.Access | ForEach-Object { $acl.RemoveAccessRule($_) } # Fjerner eksisterende rettigheder
+$inheritanceFlags = [System.Security.AccessControl.InheritanceFlags]::ContainerInherit -bor [System.Security.AccessControl.InheritanceFlags]::ObjectInherit
+$accessRule = [System.Security.AccessControl.FileSystemAccessRule]::new(
+    "File-Administration",
+    [System.Security.AccessControl.FileSystemRights]::FullControl,
+    $inheritanceFlags,
+    [System.Security.AccessControl.PropagationFlags]::None,
+    [System.Security.AccessControl.AccessControlType]::Allow
+)
+$acl.AddAccessRule($accessRule) # Tilføjer rettigheder for File-Administration
+Set-Acl -Path $folderPath -AclObject $acl # Anvender de nye rettigheder på mappen
